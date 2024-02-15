34418ada83d8f8bf8899043f28b70f3d3c8c823c
FranciscoJose28 <fcojmatos28@gmail.com>
2024-02-15 12:01:57 -0300

    inicio de objetos

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
+let teams = [];
+
 criarBtn.onclick = () => {
-    // alert("clicou")
-    // console.log("clicou aqui")
     overlay.classList.add('show');
     formCriar.classList.add("show");
 }
@@ -17,18 +17,22 @@ overlay.onclick = () => {
 
 formCriar.onsubmit = () => {
     event.preventDefault();
-    // alert(nome.value+' '+capacidade.value);
-    listTeams.innerHTML = '';
-    listTeams.innerHTML = `
-  <li>
-     <h4>${nome.value}<box-icon name="show"></box-icon></h4>
-     <h1>0 <span>/ ${capacidade.value}</span></h1>
-     <div class="actions">
-        <button>adicionar</button>
-        <button><box-icon name="trash-alt" type="solid"></box-icon></button>
-     </div>
-  </li>
-`;
+    teams.push({
+        name: nome.value,
+        capacity: capacidade.value,
+        members: []
+    });
+//     listTeams.innerHTML = '';
+//     listTeams.innerHTML = `
+//   <li>
+//      <h4>${nome.value}<box-icon name="show"></box-icon></h4>
+//      <h1>0 <span>/ ${capacidade.value}</span></h1>
+//      <div class="actions">
+//         <button>adicionar</button>
+//         <button><box-icon name="trash-alt" type="solid"></box-icon></button>
+//      </div>
+//   </li>
+// `;
     overlay.classList.remove('show');
     formCriar.classList.remove('show');
 }
