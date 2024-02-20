61ab9595393a222e2201f420ca534c1bec50fd43
FranciscoJose28 <fcojmatos28@gmail.com>
2024-02-20 12:00:03 -0300

    inicio de funções

--- js/index.js
+++ js/index.js
@@ -1,38 +1,44 @@
 let teams = [];
 
 criarBtn.onclick = () => {
-    overlay.classList.add('show');
-    formCriar.classList.add("show");
-}
+  overlay.classList.add("show");
+  formCriar.classList.add("show");
+};
 
 fecharBtn.onclick = () => {
-    overlay.classList.remove('show');
-    formCriar.classList.remove('show');
-}
+  overlay.classList.remove("show");
+  formCriar.classList.remove("show");
+};
 
 overlay.onclick = () => {
-    overlay.classList.remove('show');
-    formCriar.classList.remove('show');
-}
+  overlay.classList.remove("show");
+  formCriar.classList.remove("show");
+};
 
 formCriar.onsubmit = () => {
-    event.preventDefault();
-    teams.push({
-        name: nome.value,
-        capacity: capacidade.value,
-        members: []
-    });
-//     listTeams.innerHTML = '';
-//     listTeams.innerHTML = `
-//   <li>
-//      <h4>${nome.value}<box-icon name="show"></box-icon></h4>
-//      <h1>0 <span>/ ${capacidade.value}</span></h1>
-//      <div class="actions">
-//         <button>adicionar</button>
-//         <button><box-icon name="trash-alt" type="solid"></box-icon></button>
-//      </div>
-//   </li>
-// `;
-    overlay.classList.remove('show');
-    formCriar.classList.remove('show');
+  event.preventDefault();
+  teams.push({
+    name: nome.value,
+    capacity: capacidade.value,
+    members: [],
+  });
+  adicionarCards();
+  overlay.classList.remove("show");
+  formCriar.classList.remove("show");
+};
+
+function adicionarCards() {
+    listTeams.innerHTML = "";
+    for (let i = 0; i < teams.length; i++) {
+      listTeams.innerHTML += `
+          <li>
+             <h4>${teams[i].name}<box-icon name="show"></box-icon></h4>
+             <h1>0 <span>/ ${teams[i].capacity}</span></h1>
+             <div class="actions">
+                <button>adicionar</button>
+                <button><box-icon name="trash-alt" type="solid"></box-icon></button>
+             </div>
+          </li>
+        `;
+    }
 }
